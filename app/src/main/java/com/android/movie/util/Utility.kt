5050adabf709d610abb60c38c0cd33b09f97eeb3
Weylar <idrisaminu861@gmail.com>
2020-02-21 14:14:06 +0100

    Remove API_KEY

--- app/src/main/java/com/android/movie/util/Utility.kt
+++ app/src/main/java/com/android/movie/util/Utility.kt
@@ -9,7 +9,7 @@ object Utility {
     const val IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
     const val API_KEY = "fb97e27952573c39dd8c56b40023750e"
     const val PAGES = 100
-    const val YOUTUBE_API_KEY = "AIzaSyCy16G9NGWht68EAqAFtVm-2MTJjV4_twI"
+    const val YOUTUBE_API_KEY = ""
 
     fun hideSoftKeyBoard(context: Context, view: View) {
         try {
